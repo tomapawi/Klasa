080f6dc98a270262e68e378c38a2397f17aef474
tomapawi <32064441+tomapawi@users.noreply.github.com>
2017-11-09 10:22:05 +0100

    Add files via upload

---
+++ biblioteka/biblioteka/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace biblioteka
+{
+    public class prostokat
+    {
+        public int pole(int a, int b)
+        {
+            return a * b;
+        }
+        public int obwod(int a, int b)
+        {
+            return a + a + b + b;
+        }
+    }
+}
---
+++ mojaklasa/mojaklasa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using biblioteka;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wprowadz bok a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Wprowadz bok b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            prostokat p = new prostokat();
+            Console.WriteLine("Pole prostokata = "+p.pole(a,b));
+            Console.WriteLine("Obwod prostokata = "+p.obwod(a, b));
+            Console.ReadKey();
+        }
+    }
+}
